003d1b30339ba7a68ca0b228269db044e0623a77
tiandre jackson <drashawnyoung84@gmail.com>
2022-10-12 17:13:58 -0500

    Grade

--- controlFlowAndArrays/app.js
+++ controlFlowAndArrays/app.js
@@ -1,1 +1,21 @@
-console.log
+console.log("Hello World")
+
+
+let num = 85;
+if (num<=55){
+    console.log ("You got an F")
+}else if (num>=70) {
+    console.log ("you got an C") 
+
+    }else if (num<80){
+        console.log ("you got an C")
+    }else if (num>=80){
+        console.log ("You got an B")
+    }else if (num<90){
+        console.log ("You got an B")
+    }else (num>=90)
+    console.log ("You got an A")
+
+
+
+
